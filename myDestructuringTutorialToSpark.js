f4bcbe111d93da56696f71bb7a17b7761696f978
sobowalebukola <sobowalebukola@gmail.com>
2017-09-27 16:16:42 +0100

    Rectified the invalid output returned by the generator

--- myDestructuringTutorialToSpark.js
+++ myDestructuringTutorialToSpark.js
@@ -2,17 +2,12 @@ var sequence = {
     first_Term:5,  
     increment: function*(d) {
         var n = this.first_Term;
-         for(n; ; n++) {
-        yield n + d;
+         for( n= this.first_Term; ; n) {
+        yield n+=d;
              }
      }
-     }; 
-sequence.increment.call(sequence, 2);
-setTimeout(function () { console.log(sequence.increment.call(sequence, 2))}, 10000);
-[a,b,c,d,e, ,f,g,h,y,u,i,j,w, ,q,p,r] = sequence.increment.call(sequence, 2);
-[a,b,c,d,e, ,f,g,h,y,u,i,j,w, ,q,p,r].reduce((x,y) => x+y);
-
-
-
-
-
+     };
+     sequence.increment.call(sequence, 2);
+     setTimeout(function () { console.log(sequence.increment.call(sequence, 2))}, 10000);
+     [a,b,c,d,e, ,f,g,h,y,u,i,j,w, ,q,p,r] = sequence.increment.call(sequence, 2);
+     [a,b,c,d,e, ,f,g,h,y,u,i,j,w, ,q,p,r].reduce((x,y) => x+y);
